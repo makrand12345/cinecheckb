9f64963349a9b776b0e729216873ad8c1dae6a9a
makrand12345 <makrandgirkar43@gmail.com>
2025-11-13 11:48:03 +0530

    fixing some issues

---
+++ api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.api.routes import router as api_router
+
+app = FastAPI(title="CineCheck API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router, prefix="/api/v1")
+
+@app.get("/")
+def root():
+    return {"message": "CineCheck API is running!"}
+
+# Vercel handler
+from mangum import Mangum
+handler = Mangum(app)
