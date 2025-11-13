c5a2538f703cc75e984ed24626dee3f75f8d0a6f
makrand12345 <makrandgirkar43@gmail.com>
2025-11-13 19:56:10 +0530

    ssl issue reslove

--- src/core/db.py
+++ src/core/db.py
@@ -3,19 +3,9 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from models.movie import Movie
 from models.user import User
 from core.config import settings
-import ssl
 
 async def init_db():
-    # Create SSL context
-    ssl_context = ssl.create_default_context()
-    ssl_context.check_hostname = False
-    ssl_context.verify_mode = ssl.CERT_NONE
-    
-    client = AsyncIOMotorClient(
-        settings.MONGODB_URI,
-        tls=True,
-        tlsInsecure=True,  # Allow insecure TLS
-        ssl=ssl_context
-    )
+    # Try without SSL context
+    client = AsyncIOMotorClient(settings.MONGODB_URI)
     db = client[settings.DB_NAME]
     await init_beanie(database=db, document_models=[Movie, User])
