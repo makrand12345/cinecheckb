8ae43c72b20b808493cfb744f65baab2e43707d6
makrand12345 <makrandgirkar43@gmail.com>
2025-11-14 09:18:48 +0530

    SSL Issue

--- src/core/db.py
+++ src/core/db.py
@@ -3,19 +3,12 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from models.movie import Movie
 from models.user import User
 from core.config import settings
-import ssl
 
 async def init_db():
-    # Create SSL context (Render + Atlas fix)
-    ssl_ctx = ssl.create_default_context()
-    ssl_ctx.check_hostname = False
-    ssl_ctx.verify_mode = ssl.CERT_NONE
-
     client = AsyncIOMotorClient(
         settings.MONGODB_URI,
         tls=True,
-        tlsAllowInvalidCertificates=True,
-        ssl=ssl_ctx
+        tlsAllowInvalidCertificates=True
     )
 
     db = client[settings.DB_NAME]
